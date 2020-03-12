a26695e84b9737722f740667edd87f131c041501
biansaijun <biansaijun@hzxituan.com>
2020-03-12 09:29:30 +0800

    init

---
+++ src/pages/count/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+
+export class Count extends Component {
+  render() {
+    return (
+      <div>
+        Count
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = () => ({
+
+// })
+
+// const mapDispatchToProps = {
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Count)
+
+export default Count
