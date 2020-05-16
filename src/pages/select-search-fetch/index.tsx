8c14dbf6e2500381f641b2fa8c683963af77b31a
biansaijun <biansaijun@hzxituan.com>
2020-05-16 12:00:06 +0800

    update select-fetch

--- src/pages/select-search-fetch/index.tsx
+++ src/pages/select-search-fetch/index.tsx
@@ -1,7 +1,10 @@
 import React, { PureComponent } from 'react'
-import { Card } from 'antd'
+import { Form, Button, Card } from 'antd'
 import Panel from '@/components/panel'
 import SelectSearchFetch from '@/components/select-search-fetch'
+import { FormComponentProps } from 'antd/lib/form/Form'
+
+let defaultSelectId: number = 1
 
 const fetchData = (data: {
   page: number,
@@ -13,6 +16,9 @@ const fetchData = (data: {
     setTimeout(() => {
       const records = [...new Array(10)].map((_, i) => {
         const r = +new Date() + i
+        if (i === 0) {
+          defaultSelectId = r
+        }
         return {
           text: r + '分页',
           value: r
@@ -48,12 +54,24 @@ interface SelectSearchFetchPageState {
   value: any
 }
 
-class SelectSearchFetchPage extends PureComponent<{}, SelectSearchFetchPageState> {
+class SelectSearchFetchPage extends PureComponent<FormComponentProps, SelectSearchFetchPageState> {
   state: SelectSearchFetchPageState = {
     value: undefined
   }
 
+  handleSubmit = (e: any) => {
+    e.preventDefault()
+    this.props.form.validateFields(
+      (err: any, values: any) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      }
+    )
+  }
+
   render () {
+    const { getFieldDecorator } = this.props.form
     console.log('受控的值', this.state.value)
 
     return (
@@ -81,9 +99,38 @@ class SelectSearchFetchPage extends PureComponent<{}, SelectSearchFetchPageState
             }}
           />
         </Card>
+        <Card style={{ marginTop: 24 }} type='inner' title='在表单中使用'>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item label='选择'>
+              {getFieldDecorator('seclectId')(
+                <SelectSearchFetch
+                  api={fetchData}
+                  // allApi={fetchAll}
+                  style={{ width: '172px' }}
+                />
+              )}
+            </Form.Item>
+            <Form.Item label='选择(有默认值)'>
+              {getFieldDecorator('seclectId2', {
+                initialValue: defaultSelectId
+              })(
+                <SelectSearchFetch
+                  api={fetchData}
+                  // allApi={fetchAll}
+                  style={{ width: '172px' }}
+                />
+              )}
+            </Form.Item>
+            <Form.Item wrapperCol={{ span: 12 }}>
+              <Button type='primary' htmlType='submit'>
+                提交
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
       </Panel>
     )
   }
 }
 
-export default SelectSearchFetchPage
+export default Form.create()(SelectSearchFetchPage)
